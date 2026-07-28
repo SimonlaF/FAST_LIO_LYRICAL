d97a03b0ccdf53087f9ce4e859cf7b0e06799e34
SimonlaF <simonferrier@icloud.com>
2026-07-28 13:22:01 +0200

    Fix ROS Lyrical build compatibility
    - Link against namespaced CMake targets instead of ament_target_dependencies
    - Add missing tf2_ros dependency
    - Fix Ouster ring field type (uint8_t -> uint16_t) to match point cloud msg
    - Re-enable PCD save block in publish_frame_world

--- src/laserMapping.cpp
+++ src/laserMapping.cpp
@@ -510,10 +510,9 @@ void publish_frame_world(rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::Share
         publish_count -= PUBFRAME_PERIOD;
     }
 
-    /**************** save map ****************/
+    /**************** save map ****************
     /* 1. make sure you have enough memories
     /* 2. noted that pcd save will influence the real-time performences **/
-    /*
     if (pcd_save_en)
     {
         int size = feats_undistort->points.size();
@@ -540,7 +539,7 @@ void publish_frame_world(rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::Share
             scan_wait_num = 0;
         }
     }
-    */
+    
 }
 
 void publish_frame_body(rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pubLaserCloudFull_body)
